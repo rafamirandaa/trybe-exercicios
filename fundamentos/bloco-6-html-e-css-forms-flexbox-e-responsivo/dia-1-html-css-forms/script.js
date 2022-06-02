f35676834c3895d52d093cc46f5cbe0a9a71c013
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-06-02 07:31:39 -0300

    Adicionando botão de limpeza das infos

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -10,10 +10,3 @@ function sendInfo() {
   paragrafo.innerHTML = nome.value;
 }  
 submitButton.addEventListener('click', sendInfo)
-
-/* function removeInfo() {
-  nome.value.innerHTML = '';
-  email.value.innerHTML = 'eeeeeee';
-}
-
-clearButton.addEventListener('click', removeInfo) */
